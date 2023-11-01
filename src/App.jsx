d9fec762bac6c2577a711a44fea58579448e5bff
kawser2917 <kawserahmed8492@gmail.com>
2023-11-01 12:16:49 +0600

    fake json data created with chatgpt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl bg-red-400'>Knowledge Cafe</h1>
-       
+     <Header></Header>
     </>
   )
 }
